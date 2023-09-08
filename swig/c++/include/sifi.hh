3d76388a3cba909e1980a4c650b5b1047161fd53
Mason Katz <Mason.Katz@SoftIron.com>
2023-09-08 16:07:34 -0700

    feat: add semver version to c++ code

--- swig/c++/include/sifi.hh
+++ swig/c++/include/sifi.hh
@@ -1,9 +1,14 @@
 #ifndef __sifi_hh
 #define __sifi_hh
 
+#ifndef SIFI_VERSION
+#	define SIFI_VERSION "undefined"
+#endif
+
 namespace sifi
 {
-	const int PortNumber = 7434;
-}
+	const int   PortNumber = 7434;
+	const char *Version    = SIFI_VERSION;
+} // namespace sifi
 
 #endif // __sifi_hh
