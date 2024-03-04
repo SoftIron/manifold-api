ee69525befe3cb6fd4f6c9254dc681652faa0f98
Mason Katz <Mason.Katz@SoftIron.com>
2024-03-04 12:01:40 -0800

    refactor: ResponseError Detail -> Details

--- sifi/responses.go
+++ sifi/responses.go
@@ -8,24 +8,24 @@ import (
 
 // ResponseError is the response body for all error responses.
 type ResponseError struct {
-	Code   int      `json:"code"`
-	Text   string   `json:"error"`
-	Detail []string `json:"detail"`
+	Code    int      `json:"code"`
+	Text    string   `json:"error"`
+	Details []string `json:"details"`
 }
 
 // NewErrorResponse returns a new ErrorResponse.
 func NewErrorResponse(code int, err error, detail ...string) *ResponseError {
 	return &ResponseError{
-		Code:   code,
-		Text:   err.Error(),
-		Detail: detail,
+		Code:    code,
+		Text:    err.Error(),
+		Details: detail,
 	}
 }
 
 // Error implements the error interface for e.
 func (e ResponseError) Error() string {
 	s := fmt.Sprintf("%s (%d) - %s", strings.ToLower(http.StatusText(e.Code)), e.Code, e.Text)
-	for _, d := range e.Detail {
+	for _, d := range e.Details {
 		s += "\n\t" + d
 	}
 
